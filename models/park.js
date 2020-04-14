990b9441d4b56f317117e51f5a0a33dd722fe3ec
Jack Quigley <jackaquigley@gmail.com>
2020-04-14 19:45:27 +0100

    built park constructer

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, ticketPrice, dinosaurArray) {
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.dinosaurArray = dinosaurArray;
+}
+
+module.exports = Park;
